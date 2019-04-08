33faa86c417cbe1f9b003463b3abb0f968f45e3a
liuhll <1029765111@qq.com>
2019-04-08 16:40:56 +0800

    新增product微服务组件

---
+++ hl-services/hl-product/hl-product-server/src/main/java/com/liuhll/hl/product/ProductApplication.java
@@ -0,0 +1,11 @@
+package com.liuhll.hl.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringCloudApplication
+public class ProductApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class,args);
+    }
+}
