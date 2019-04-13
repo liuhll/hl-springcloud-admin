3ec62a19cd0d15e89e6b64c8417113f62807505a
liuhongliang <1029765111@qq.com>
2019-04-13 17:22:07 +0800

    update swagger config

--- hl-services/hl-product/hl-product-server/src/main/java/com/liuhll/hl/product/conf/Swagger2Config.java
+++ hl-services/hl-product/hl-product-server/src/main/java/com/liuhll/hl/product/conf/Swagger2Config.java
@@ -1,7 +1,5 @@
 package com.liuhll.hl.product.conf;
 
-import com.google.common.base.Strings;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -9,13 +7,8 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.paths.AbstractPathProvider;
-import springfox.documentation.spring.web.paths.RelativePathProvider;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger.web.*;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import javax.servlet.ServletContext;
 
 @Configuration
 @EnableSwagger2
@@ -30,7 +23,6 @@ public class Swagger2Config {
                 .paths(PathSelectors.any())
                 .build()
                 .pathMapping("/")
-                .pathProvider(relativeSwaggerPathProvider())
                 ;
     }
 
@@ -41,57 +33,6 @@ public class Swagger2Config {
                 .description("product server api docs")
                 .version("v1.0.0")
                 .build();
-    }
-
-    @Autowired
-    private ServletContext servletContext;
-
-    @Bean
-    public AbstractPathProvider relativeSwaggerPathProvider() {
-        return new ApiRelativeSwaggerPathProvider(servletContext);
-    }
-
-
-    private class ApiRelativeSwaggerPathProvider extends RelativePathProvider {
-        public static final String ROOT = "/";
-        private final ServletContext servletContext;
-
-        public ApiRelativeSwaggerPathProvider(ServletContext servletContext) {
-            super(servletContext);
-            this.servletContext = servletContext;
-        }
-
-        protected String applicationPath() {
-            return Strings.isNullOrEmpty(this.servletContext.getContextPath()) ? "/" : this.servletContext.getContextPath();
-        }
-
-        protected String getDocumentationPath() {
-            // return "/";
-            return Strings.isNullOrEmpty(this.servletContext.getContextPath()) ? "/" : this.servletContext.getContextPath();
-        }
-    }
-
-
-    @Bean
-    UiConfiguration uiConfig() {
-        UiConfiguration uiConfiguration = UiConfigurationBuilder.builder()
-                .deepLinking(true)
-                .displayOperationId(false)
-                .defaultModelsExpandDepth(1)
-                .defaultModelExpandDepth(1)
-                .defaultModelRendering(ModelRendering.EXAMPLE)
-                .displayRequestDuration(false)
-                .docExpansion(DocExpansion.LIST)
-                .filter(false)
-                .maxDisplayedTags(null)
-                .operationsSorter(OperationsSorter.ALPHA)
-                .showExtensions(true)
-                .tagsSorter(TagsSorter.ALPHA)
-                .supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS)
-                .validatorUrl("/")
-                .build();
-
-        return uiConfiguration;
     }
 
 
