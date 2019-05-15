8d87d338d0ce700247553228674b9b2a76f8ab3f
刘洪亮 <liuhongliang@wintelia.com>
2019-05-15 10:51:20 +0800

    更新UserGroup表设计,新增GroupCode字段

--- docs/sql/hl_auth.sql
+++ docs/sql/hl_auth.sql
@@ -329,15 +329,14 @@ create table auth_user
 alter table auth_user comment '用户表';
 
 
-alter table auth_user comment '用户表';
-
 /*==============================================================*/
 /* Table: auth_user_group                                       */
 /*==============================================================*/
 create table auth_user_group
 (
    Id                   bigint not null auto_increment comment '主键',
-   ParentId             varchar(22) not null comment '父用户组Id',
+   ParentId             bigint not null comment '父用户组Id',
+   GroupCode            varchar(50),
    GroupName            varchar(50) not null comment '用户组名称',
    Status               int not null comment '状态',
    CreateBy             bigint comment '创建人',
